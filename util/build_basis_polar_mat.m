d4b3abd2bca8bd147e9feaa45873e3d5ab9bbf15
Tommy Sprague <tommy.sprague@gmail.com>
2018-06-29 18:03:27 -0400

    moved build_basis_polar.m to util/

--- util/build_basis_polar_mat.m
+++ util/build_basis_polar_mat.m
@@ -1,13 +1,27 @@
-function bb = build_basis_polar_mat(evalAt,rfTh)
+function bb = build_basis_polar_mat(eval_at,chan_th,chan_size,chan_pow )
 % adapted from build_basis_polar - this does everything at once, useful for
 % generating a set of transformed/rotated basis functions
 
 % will return bb, length(evalAt) x length(rfTh)
-n_basis = length(rfTh);
-[rfTh,evalAt] = meshgrid(squeeze(rfTh),squeeze(evalAt));
+n_basis = length(chan_th);
+[chan_th,eval_at] = meshgrid(squeeze(chan_th),squeeze(eval_at));
 
-%bb = nan(length(rfTh),1);
-%for ii = 1:length(rfTh)
-    bb = cosd(0.5*min(mod(evalAt-rfTh,360),mod(rfTh-evalAt,360))).^(n_basis-mod(n_basis,2));
-%end
+% TODO: make sure only one chan_size...
+
+
+if nargin < 3 || isempty(chan_size)
+    chan_size = 180; % default value
+end
+
+if nargin < 4 || isempty(chan_pow)
+    chan_pow = n_basis-mod(n_basis,2);
+end
+
+% utility function to compute distance between two angles
+ang_dist = @(a,b) min(mod(a-b,360),mod(b-a,360));
+
+
+bb = (cosd( 180 * ang_dist(eval_at,chan_th) ./ (2*chan_size) ).^chan_pow) .* (ang_dist(eval_at,chan_th)<=chan_size) ;
+
+
 return
